bd990c9e828fa15c15910ec37d029297ff338371
TimothyLetsoko <107189293+TimothyLetsoko@users.noreply.github.com>
2023-04-01 21:50:44 +0200

    Changed to prepared statements to allow for input of data using scanner object.

--- insertEmployee/src/Main.java
+++ insertEmployee/src/Main.java
@@ -1,22 +1,44 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //create url
         String url = "JDBC:mysql://127.0.0.1:3306/employee_database";
 
 
-        try {
-            //get connection
-            Connection conn = DriverManager.getConnection(url,"root","Root");
+        try{
+            System.out.print("Enter employee id:");
+            int id = scanner.nextInt();
 
-            //create statement
-            Statement statement = conn.createStatement();
+            System.out.print("Enter employee name:");
+            String name = scanner.next();
 
-            //execute query
-            int rowsAffected = statement.executeUpdate("INSERT INTO employees(name, dept, salary) " +
-                    "VALUES('France', 'Sales',10000)");
-            System.out.println("Insertion success, "+rowsAffected+" affected.");
+            System.out.print("Enter department name:");
+            String department = scanner.next();
+
+            System.out.print("Enter salary amount:");
+            double salary = scanner.nextDouble();
+
+            //create connection
+            Connection connection = DriverManager.getConnection(url,"root","Root");
+
+            //create preparedStatement
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employees(id, name, dept, salary) " +
+                    "VALUES(?,?,?,?)");
+
+            preparedStatement.setInt(1,id);
+            preparedStatement.setString(2,name);
+            preparedStatement.setString(3,department);
+            preparedStatement.setDouble(4,salary);
+
+            //execute update
+            preparedStatement.executeUpdate();
+
+            connection.close();
+            scanner.close();
+            preparedStatement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
